ed8237e3895e8f9cd23c1d8c84f995e72fb0e6ce
Fabio Henrique <fabiohenrique3@gmail.com>
2013-02-05 11:50:33 -0200

    Commit Fabio 1

---
+++ cadastro_tarefas.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Cadastro Tarefas</title>
+</head>
+<body>
+  <h2>Cadastro</h2>
+
+<?php
+  $data = $_POST['data'];
+  $descricao = $_POST['descricao'];
+  
+  echo "Cadastro realizado com sucesso!";
+  
+?>
+
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+</head>
+<body>
+	<h1>Login</h1>
+<form method="POST">
+<label for="login">Login</label>
+<input type="text"id="login" name="login"/>
+<label for="senha">Senha</label>
+<input type="text"id="senha" name="senha"/>
+<input type="submit" value="Entrar" name="submit" />
+
+</form>
+
+
+</body>
+
+</html>
